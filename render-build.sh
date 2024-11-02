a88de3a3b02d2e673b03e698f598eca6f5d6aaaf
Nithish <nithishkumar.gujjari@ktechproducts.com>
2024-11-01 19:22:17 -0500

    Remove sudo from render-build.sh for Render environment

--- render-build.sh
+++ render-build.sh
@@ -2,11 +2,11 @@
 
 # Install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt-get -f install -y
+dpkg -i google-chrome-stable_current_amd64.deb || apt-get -f install -y
 
 # Install ChromeDriver
 CHROME_DRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
+mv chromedriver /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
